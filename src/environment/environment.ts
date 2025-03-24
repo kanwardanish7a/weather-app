5b8794ee0a25c0b96259cf41102c5dc6766182d7
KanwarDanish <gitoutsidecolab@zodiac.tech>
2025-03-24 19:20:35 -0400

    api update

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7167' // Local API URL
-    // apiUrl: 'https://weather-api-kanwardanish-d5bvdgemfjgtb6dd.canadacentral-01.azurewebsites.net' // Local API URL
+    // apiUrl: 'https://localhost:7167' // Local API URL
+    apiUrl: 'https://weather-api-kanwardanish-d5bvdgemfjgtb6dd.canadacentral-01.azurewebsites.net' // Local API URL
   };
